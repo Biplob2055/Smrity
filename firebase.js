3f894f77402c176f657915998c61924d16474493
Biplob2055 <45505092+Biplob2055@users.noreply.github.com>
2026-07-14 23:59:22 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,15 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp, deleteDoc, doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ⚠️ এখানে আপনার ফায়ারবেস কনসোলের আসল ক্রেডেনশিয়াল বসাবেন
-const firebaseConfig = {
 const firebaseConfig = {
   apiKey: "AIzaSyC_ThrBO6EoEX9WIEzM63oXPA9tHYwpSws",
-  authDomain: "govjob.vercel.app", // 👈 এখানে আপনার নতুন ডোমেইনের নাম বসান (https:// ছাড়া)
+  authDomain: "govjob.vercel.app", 
   projectId: "smrity-chat",
   storageBucket: "smrity-chat.firebasestorage.app",
   messagingSenderId: "979577122342",
   appId: "1:979577122342:web:b63b79ad6b9745b94080e7",
   measurementId: "G-KRDKRNS8B4"
-};
 };
 
 const app = initializeApp(firebaseConfig);
